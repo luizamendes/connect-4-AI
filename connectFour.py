a014599355f8c5b82e5a2bd4766d8ffe766bb1fe
isabela wolff <isabelawolffc@gmail.com>
2020-06-12 21:49:20 -0300

    tentativa escolha poda

--- connectFour.py
+++ connectFour.py
@@ -338,7 +338,7 @@ pygame.display.set_caption("Lig-4")
 myfont = pygame.font.SysFont("monospace", 75)
 
 
-def start(value, difficulty=0):
+def start(value, difficulty=0, withOrWithoutPodaAB=0):
 	if difficulty == 2:
 		board = create_board(True)
 	else:
@@ -441,23 +441,42 @@ def start(value, difficulty=0):
 
 		# # Human turn
 		if turn == AI and not game_over:
-			col, minimax_score = negamaxAB(board, 5, AI_PIECE, -math.inf, math.inf)
-			# col, minimax_score = minimaxAB(board, 5, -math.inf, math.inf, True)
+			if withOrWithoutPodaAB == 1:
+				col, minimax_score = negamaxAB(board, 5, AI_PIECE, -math.inf, math.inf)
+				# col, minimax_score = minimaxAB(board, 5, -math.inf, math.inf, True)
 
-			if is_valid_location(board, col):
-				row = get_next_open_row(board, col)
-				drop_piece(board, row, col, AI_PIECE)
+				if is_valid_location(board, col):
+					row = get_next_open_row(board, col)
+					drop_piece(board, row, col, AI_PIECE)
 
-				if winning_move(board, AI_PIECE):
-					label = myfont.render("NEGAMAX VENCEU!!!", 1, YELLOW)
-					screen.blit(label, (40,10))
-					game_over = True
+					if winning_move(board, AI_PIECE):
+						label = myfont.render("NEGAMAX VENCEU!!!", 1, YELLOW)
+						screen.blit(label, (40,10))
+						game_over = True
 
-				print_board(board)
-				draw_board(board)
+					print_board(board)
+					draw_board(board)
 
-				turn += 1
-				turn = turn % 2
+					turn += 1
+					turn = turn % 2
+			elif withOrWithoutPodaAB == 2:
+				col, minimax_score = negamax(board, 5, AI_PIECE)
+				# col, minimax_score = minimaxAB(board, 5, -math.inf, math.inf, True)
+
+				if is_valid_location(board, col):
+					row = get_next_open_row(board, col)
+					drop_piece(board, row, col, AI_PIECE)
+
+					if winning_move(board, AI_PIECE):
+						label = myfont.render("NEGAMAX VENCEU!!!", 1, YELLOW)
+						screen.blit(label, (40,10))
+						game_over = True
+
+					print_board(board)
+					draw_board(board)
+
+					turn += 1
+					turn = turn % 2
 
 		if game_over:
 			print('entrou')
--- menu.py
+++ menu.py
@@ -7,10 +7,10 @@ verde = (0,255,0)
 azul = (0,0,255)
 
 try:
-    pygame.init()
-    pygame.font.init()
+	pygame.init()
+	pygame.font.init()
 except:
-    print("O modulo pygame não foi inicializado com sucesso")
+	print("O modulo pygame não foi inicializado com sucesso")
 
 largura = 640
 altura = 480
@@ -47,59 +47,80 @@ AI_X_AI_flag = False
 next_step = 0
 
 while sair:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            sair = False
-    
-    fundo.fill(branco)
-    fundo.blit(menu_step_text_render, (200, 100))
-    fundo.blit(text_1_render,(200, 170))
-    fundo.blit(text_2_render,(200, 200))
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			sair = False
+	
+	fundo.fill(branco)
+	fundo.blit(menu_step_text_render, (200, 100))
+	fundo.blit(text_1_render,(200, 170))
+	fundo.blit(text_2_render,(200, 200))
 
-    if event.type == pygame.KEYDOWN:# 1=49, 2=50
-        if next_step == 0:
-            if event.key == 49:
-                menu_choices['choice_1'] = 1
-            if event.key == 50:
-                menu_choices['choice_1'] = 2
-                next_step = 1
-            if event.key == 13:
-                from connectFour import start
-                start(menu_choices['choice_1'])
+	if event.type == pygame.KEYDOWN:# 1=49, 2=50
+		if next_step == 0:
+			if event.key == 49:
+				menu_choices['choice_1'] = 1
+			if event.key == 50:
+				menu_choices['choice_1'] = 2
+				next_step = 1
+			if event.key == 13:
+				from connectFour import start
+				start(menu_choices['choice_1'])
 
-        elif next_step == 1:
+		elif next_step == 1:
             if event.key == 51:
                 menu_choices['choice_2'] = 1
                 next_step = 2      
             if event.key == 52:
                 menu_choices['choice_2'] = 2
                 next_step = 2
+
         elif next_step == 2:
+            if event.key == 53:
+                menu_choices['choice_3'] = 1
+                next_step = 3      
+            if event.key == 54:
+                menu_choices['choice_3'] = 2
+                next_step = 3
+        
+        elif next_step == 3:
             if event.key == 13:
                 from connectFour import start
-                start(menu_choices['choice_1'], menu_choices['choice_2'])
+                start(menu_choices['choice_1'], menu_choices['choice_2'], menu_choices['choice_3'])
 
-    if menu_choices['choice_1'] == 1:
-        choice_1_text = 'Modo: AI contra AI'
-        fundo.blit(start_game_text_render, (180, 300))
-        choice_1_text_render = font.render(choice_1_text, True, azul)
-        fundo.blit(choice_1_text_render,(220, 230))
-        text_1_render = font.render(text_1, True, branco)
-        text_2_render = font.render(text_2, True, branco)
+	if menu_choices['choice_1'] == 1:
+		choice_1_text = 'Modo: AI contra AI'
+		fundo.blit(start_game_text_render, (180, 300))
+		choice_1_text_render = font.render(choice_1_text, True, azul)
+		fundo.blit(choice_1_text_render,(220, 230))
+		text_1_render = font.render(text_1, True, branco)
+		text_2_render = font.render(text_2, True, branco)
 
-    if next_step == 1:
+	if next_step == 1:
+		if menu_choices['choice_1'] == 2:
+			choice_1_text = 'Humano contra AI'
+			menu_step_text = "Nível de Dificuldade"
+			menu_step_text_render = menu_step_font.render(menu_step_text, True, azul)
+			text_1 = '3 - Fácil'
+			text_2 = '4 - Difícil'
+			text_1_render = font.render(text_1, True, vermelho)
+			text_2_render = font.render(text_2, True, vermelho)
+		choice_1_text_render = font.render(choice_1_text, True, azul)
+		fundo.blit(choice_1_text_render,(200, 300))
+	
+	    if next_step == 2:
         if menu_choices['choice_1'] == 2:
             choice_1_text = 'Humano contra AI'
-            menu_step_text = "Nível de Dificuldade"
+            menu_step_text = "Contra qual IA?"
             menu_step_text_render = menu_step_font.render(menu_step_text, True, azul)
-            text_1 = '3 - Fácil'
-            text_2 = '4 - Difícil'
+            text_1 = '5 - Com poda alfa beta pruning'
+            text_2 = '6 - Sem poda alfa beta pruning'
             text_1_render = font.render(text_1, True, vermelho)
             text_2_render = font.render(text_2, True, vermelho)
         choice_1_text_render = font.render(choice_1_text, True, azul)
         fundo.blit(choice_1_text_render,(200, 300))
     
-    if next_step == 2:
+    if next_step == 3:
         if menu_choices['choice_2'] == 1:
             choice_2_text = 'Nível fácil'
         if menu_choices['choice_2'] == 2:
@@ -110,8 +131,8 @@ while sair:
         fundo.blit(choice_2_text_render,(220, 260))
         text_1_render = font.render(text_1, True, branco)
         text_2_render = font.render(text_2, True, branco)
-                    
-    pygame.display.update()
-    
+					
+	pygame.display.update()
+	
 pygame.quit()
 quit()
