c6bb0bbc7d5e8039051480555adc3b5e69d2e79c
Luiza <luizaleitemendes@gmail.com>
2020-06-17 20:18:06 -0300

    Corrigindo indentacao

--- connectFour.py
+++ connectFour.py
@@ -447,46 +447,46 @@ def start(value, difficulty=0, withWithoutPoda=0):
 
 		# AI Negamax turn
 		if turn == AI and not game_over:
-    			if withWithoutPoda == 1:
-					print('negamax depth', depth)
-					col, minimax_score = negamaxAB(board, depth, AI_PIECE, -math.inf, math.inf)
-					# col, minimax_score = minimaxAB(board, 5, -math.inf, math.inf, True)
+			if withWithoutPoda == 1:
+				print('negamax depth', depth)
+				col, minimax_score = negamaxAB(board, depth, AI_PIECE, -math.inf, math.inf)
+				# col, minimax_score = minimaxAB(board, 5, -math.inf, math.inf, True)
 
-					if is_valid_location(board, col):
-						row = get_next_open_row(board, col)
-						drop_piece(board, row, col, AI_PIECE)
+				if is_valid_location(board, col):
+					row = get_next_open_row(board, col)
+					drop_piece(board, row, col, AI_PIECE)
 
-						if winning_move(board, AI_PIECE):
-							label = myfont.render("NEGAMAX VENCEU!!!", 1, YELLOW)
-							screen.blit(label, (40,10))
-							game_over = True
+					if winning_move(board, AI_PIECE):
+						label = myfont.render("NEGAMAX VENCEU!!!", 1, YELLOW)
+						screen.blit(label, (40,10))
+						game_over = True
 
-						print_board(board)
-						draw_board(board)
+					print_board(board)
+					draw_board(board)
 
-						turn += 1
-						turn = turn % 2
+					turn += 1
+					turn = turn % 2
 
-				elif withWithoutPoda == 2:
-    				print('negamax depth', depth)
-					col, minimax_score = negamax(board, depth, AI_PIECE)
-					# col, minimax_score = minimaxAB(board, 5, -math.inf, math.inf, True)
+			elif withWithoutPoda == 2:
+				print('negamax depth', depth)
+				col, minimax_score = negamax(board, depth, AI_PIECE)
+				# col, minimax_score = minimaxAB(board, 5, -math.inf, math.inf, True)
 
-					if is_valid_location(board, col):
-						row = get_next_open_row(board, col)
-						drop_piece(board, row, col, AI_PIECE)
+				if is_valid_location(board, col):
+					row = get_next_open_row(board, col)
+					drop_piece(board, row, col, AI_PIECE)
 
-						if winning_move(board, AI_PIECE):
-							label = myfont.render("NEGAMAX VENCEU!!!", 1, YELLOW)
-							screen.blit(label, (40,10))
-							game_over = True
+					if winning_move(board, AI_PIECE):
+						label = myfont.render("NEGAMAX VENCEU!!!", 1, YELLOW)
+						screen.blit(label, (40,10))
+						game_over = True
 
-						print_board(board)
-						draw_board(board)
+					print_board(board)
+					draw_board(board)
 
-						turn += 1
-						turn = turn % 2
-    				
+					turn += 1
+					turn = turn % 2
+				
 
 		if game_over:
 			pygame.quit()
